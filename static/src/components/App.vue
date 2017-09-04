20552a43994a3d342b76b8b45e17ff935c93152f
Sebastian Schulz <schulz.siwy@gmail.com>
2017-09-04 20:16:46 +0200

    map and search components

--- static/src/components/App.vue
+++ static/src/components/App.vue
@@ -1,87 +1,58 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#">Instaloc</a>
+      <a class="navbar-brand" href="/">Instaloc</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto"></ul>
-        <form class="form-inline my-2 my-lg-0" @submit.prevent>
-          <input class="form-control mr-sm-2" type="text" placeholder="Instagram nickname" aria-label="Search" v-model="nick">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit" @click="runWebsocket" :disabled="!button">Search</button>
-        </form>
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <search-box
+          :searching="searching"
+          @search="searchProfile">
+        </search-box>
       </div>
     </nav>
-    <div id="map" style="height:100%; width: 100%"></div>
+    <instagram-map
+      ref="instagramMap">
+    </instagram-map>
   </div>
 </template>
 
 <script>
-import ol from 'openlayers'
+import SearchBox from './SearchBox.vue'
+import InstagramMap from './InstagramMap.vue'
+
 export default {
   data() {
     return {
-      nick: '_schulzsebastian',
-      button: true
+      searching: false
     }
   },
+  components: {
+    SearchBox,
+    InstagramMap
+  },
   methods: {
-    getData() {
-      fetch('/data')
-      .then(resp => resp.json())
-      .then(data => {
-        alert(data.data)
-      })
-    },
-    runWebsocket() {
-      const ws = new WebSocket("ws://localhost:5000/websocket")
+    searchProfile(nickname) {
+      let url = location.host
+      if(location.host == "localhost:5001") url = "localhost:5000"
+      const ws = new WebSocket(`ws://${url}/websocket`)
       ws.onopen = () => {
-        this.button = false
-        if(this.source) this.source.clear()
-        this.source = new ol.source.Vector()
-        this.layer = new ol.layer.Vector({
-          source: this.source,
-        })
-        this.map.addLayer(this.layer)
-        ws.send(this.nick)
+        this.searching = true
+        this.$refs.instagramMap.clearMap()
+        ws.send(nickname)
       }
       ws.onmessage = evt => {
         if(evt.data == "end") ws.close()
-        else this.addPoint(JSON.parse(evt.data))
+        else this.$refs.instagramMap.addPoint(JSON.parse(evt.data))
       }
       ws.onclose = () => {
-        this.button = true
-        this.map.getView().fit(this.source.getExtent(), this.map.getSize());
+        this.searching = false
+        this.$refs.instagramMap.fitToLayer()
       }
-    },
-    addPoint(point) {
-      let feature = new ol.Feature({
-        geometry: new ol.geom.Point(ol.proj.transform([point.lng, point.lat], 'EPSG:4326', 'EPSG:3857'))
-      })
-      this.source.addFeature(feature)
     }
-  },
-  mounted() {
-    this.map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.XYZ({
-            urls: [
-              'http://mt0.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-              'http://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-              'http://mt2.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-              'http://mt3.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-            ]
-          })
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([16.9, 52.4]),
-        zoom: 3
-      })
-    })
   }
 }
 </script>
